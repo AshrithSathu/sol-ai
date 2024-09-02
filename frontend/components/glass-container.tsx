c30c32f14f4025b3db25ff56702fc417a99e76aa
AshrithSathu <sathuashrith@gmail.com>
2024-09-02 18:47:20 +0530

    data = string

--- frontend/components/glass-container.tsx
+++ frontend/components/glass-container.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function GlassContainer({ data }) {
+export default function GlassContainer({ data }: { data: string }) {
   return (
     <div className="absolute inset-0 flex items-center justify-center">
       <div className="h-5/6 w-5/6 rounded-lg bg-white bg-opacity-5 p-4 text-white backdrop-blur-lg">
